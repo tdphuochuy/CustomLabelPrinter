0b3727de4a1e107b780e94d61ef6fb6b4344d1ae
tdphu <tdphu@moneymine>
2025-02-10 08:20:24 -0500

    adjusted config file

--- CustomLabelPrinter/src/config/Config.java
+++ CustomLabelPrinter/src/config/Config.java
@@ -3,6 +3,6 @@ public class Config {
     // Static global variable
     public static String printerIP = "167.110.88.226";
     public static String printer2IP = "167.110.88.245";
-    public static boolean chatServer = true;
     public static String chatId = "DSI ERP";
+    public static boolean chatServer = true;
 }
