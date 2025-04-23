ea4d88df0ed01270657afda0e195e6d381395946
tdphu <tdphu@moneymine>
2025-04-23 14:09:43 -0400

    added tooltip to preview button

--- CustomLabelPrinter/src/tpCount/CaseType.java
+++ CustomLabelPrinter/src/tpCount/CaseType.java
@@ -76,6 +76,7 @@ public class CaseType extends JPanel{
         
         // Create the button with the icon
         JButton previewButton = new JButton(IconFontSwing.buildIcon(FontAwesome.QUESTION_CIRCLE, 15,Color.decode("#69a5de")));
+        previewButton.setToolTipText("Preview split result");
         previewButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
         previewButton.setMaximumSize(new Dimension(15,15));
         previewButton.setPreferredSize(new Dimension(15,15));
